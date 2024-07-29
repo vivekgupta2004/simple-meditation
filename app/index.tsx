1ed687efcc037442946ac5a56291c55d70a7bd03
mr_varshney2004 <130595295+vivekgupta2004@users.noreply.github.com>
2024-07-29 15:26:21 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, View , Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ container:{
+  flex:1,
+  justifyContent:'center',
+  alignItems:'center',
+  backgroundColor:'#F5FCFF'
+  
+ }
+});
