105d7400d46f418368064edec3fc63c9c5794b67
mr_varshney2004 <130595295+vivekgupta2004@users.noreply.github.com>
2024-07-30 16:42:32 +0530

    create customButton and  tailwind _layout home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,43 @@
-import { StyleSheet, View , Text } from 'react-native';
+import { View, Text, ImageBackground, SafeAreaView, } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import beachImage from "@/assets/meditation-images/beach.webp"
 
+import { StatusBar } from 'expo-status-bar'
+import CustomButton from '@/components/CustomButton'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>
-        Hello World
-      </Text>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <View className='flex-1 '>
+      <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
+        <LinearGradient className='flex-1' colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+
+          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
+            <View>
+              <Text className='text-center text-white font-bold text-4xl'>
+                Simple Meditation
+              </Text>
+              <Text className='text-center text-white text-regular text-2xl mt-3'>
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+         <CustomButton onPress={()=>console.log('tap')} title="Get Started"/>
+
+            </View>
+
+            <StatusBar style='light' />
+          </SafeAreaView>
+        </LinearGradient>
+
+      </ImageBackground>
+
     </View>
-  );
+    </GestureHandlerRootView>
+  )
 }
 
-const styles = StyleSheet.create({
- container:{
-  flex:1,
-  justifyContent:'center',
-  alignItems:'center',
-  backgroundColor:'#F5FCFF'
-  
- }
-});
+export default App
