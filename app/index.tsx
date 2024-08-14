33ae9b425a8b8a6d6d76844b6c75171b3691bbe8
mr_varshney2004 <130595295+vivekgupta2004@users.noreply.github.com>
2024-08-14 18:01:18 +0530

    creaet appGradient

--- app/index.tsx
+++ app/index.tsx
@@ -6,36 +6,40 @@ import beachImage from "@/assets/meditation-images/beach.webp"
 import { StatusBar } from 'expo-status-bar'
 import CustomButton from '@/components/CustomButton'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useRouter } from 'expo-router'
+import AppGradient from '@/components/AppGradient'
 
 const App = () => {
+  const router = useRouter()
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-    <View className='flex-1 '>
-      <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
-        <LinearGradient className='flex-1' colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+      <View className='flex-1 '>
+        <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
+          <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+            
 
-          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
-            <View>
-              <Text className='text-center text-white font-bold text-4xl'>
-                Simple Meditation
-              </Text>
-              <Text className='text-center text-white text-regular text-2xl mt-3'>
-                Simplifying Meditation for Everyone
-              </Text>
-            </View>
+              <SafeAreaView className='flex-1 px-1 justify-between'>
+                <View>
+                  <Text className='text-center text-white font-bold text-4xl'>
+                    Simple Meditation
+                  </Text>
+                  <Text className='text-center text-white text-regular text-2xl mt-3'>
+                    Simplifying Meditation for Everyone
+                  </Text>
+                </View>
 
-            <View>
-         <CustomButton onPress={()=>console.log('tap')} title="Get Started"/>
+                <View>
+                  <CustomButton onPress={() => router.push('/nature-meditate')} title="Get Started" />
 
-            </View>
+                </View>
 
-            <StatusBar style='light' />
-          </SafeAreaView>
-        </LinearGradient>
+                <StatusBar style='light' />
+              </SafeAreaView>
+            
+          </AppGradient>
+        </ImageBackground>
 
-      </ImageBackground>
-
-    </View>
+      </View>
     </GestureHandlerRootView>
   )
 }
