d227072c701ba57ac6343187d72ddf4ff2d4a85c
mr_varshney2004 <130595295+vivekgupta2004@users.noreply.github.com>
2024-08-24 17:14:17 +0530

    audio put

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -6,6 +6,8 @@ import { router, useLocalSearchParams } from 'expo-router'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import CustomButton from '@/components/CustomButton'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { Audio } from 'expo-av'
+import { MEDITATION_DATA, AUDIO_FILES } from '@/constants/MeditationData'
 
 
 
@@ -15,6 +17,9 @@ const Meditate = () => {
     const { id } = useLocalSearchParams();
     const [secondsRemaining, setSecondsRemaining] = useState(12);
     const [isMeditating, setMeditating] = useState(false);
+    const [audioSound, setSound] = useState<Audio.Sound>();
+
+    const [isPlayingAudio, setPlayingAudio] = useState(false);
 
 
     useEffect(() => {
@@ -36,10 +41,51 @@ const Meditate = () => {
         }
     }, [secondsRemaining, isMeditating])
 
+    useEffect(() => {
+        return () => {
+            audioSound?.unloadAsync()
+        }
+    }, [audioSound])
+
+    const toggleMeditationSessionStatus = async () => {
+        if (secondsRemaining === 0) setSecondsRemaining(10);
+
+        setMeditating(!isMeditating)
+
+        await toggleSound()
+    }
+    const toggleSound = async () => {
+        const sound = audioSound ? audioSound : await initializeSound();
+
+       
+        const status = await sound?.getStatusAsync();
+        if (status?.isLoaded && !isPlayingAudio) {
+            await sound.playAsync();
+            setPlayingAudio(true);
+
+        }
+        
+        else {
+            await sound.pauseAsync();
+            setPlayingAudio(false)
+        }
+
+    }
+    const initializeSound = async () => {
+
+        const audioFileName = MEDITATION_DATA[Number(id) - 1].audio;
+        const { sound } = await Audio.Sound.createAsync(
+            AUDIO_FILES[audioFileName]
+        )
+        setSound(sound);
+        return sound
+
+    }
+
     //Format the time left to ensure two digit  are displayed
 
-    const formattedTimeMinutes = String(Math.floor(secondsRemaining/60)).padStart(2,"0");
-    const formattedTimeSeconds = String(secondsRemaining%60).padStart(2, "0")
+    const formattedTimeMinutes = String(Math.floor(secondsRemaining / 60)).padStart(2, "0");
+    const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0")
 
     return (
         <View className='flex-1'>
@@ -58,7 +104,7 @@ const Meditate = () => {
                     </View>
                     <GestureHandlerRootView className='mb-5'>
                         <View>
-                            <CustomButton title='Start Meditation' onPress={() => setMeditating(true)} />
+                            <CustomButton title='Start Meditation' onPress={toggleMeditationSessionStatus} />
 
                         </View>
                     </GestureHandlerRootView>
